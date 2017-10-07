f48c72bc1ae595e79eca36dc9db343c2b115bcbd
afshantazeena <afshantazeena@gmail.com>
2017-10-07 15:12:05 -0400

    upadte

--- about.php
+++ about.php
@@ -1,3 +1,9 @@
+<?php 
+	
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -13,45 +19,8 @@
 		<link rel="stylesheet" type="text/css" href="aboutstyle.css" media="all" />
 	</head>
 	<body>
-		<div class="container-fluid">
-			<nav class="navbar navbar-expand-lg navbar-light ">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-					<a class="navbar-brand" href="#">exclusivewallcovering</a>
-					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">ABOUT</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link " href="#">SERVICES</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">PHOTOGRAPHY</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link " href="#">LATEST NEWS</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link " href="#">CONTACT US</a>
-						</li>
-					</ul>
-					
-				</div>
-			</nav>
-		</div>
-		<div class="container-fluid1">
-			<div class="row"> 
-				<div class="col-sm-12"> 
-					<h2>EXCLUSIVE WALLCOVERING</h2>
-					
-				</div>
-			</div>
-		</div>
+	<?php include ("includes/menu.html");?>
+		
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-8">
@@ -107,10 +76,10 @@
 			<div class="row"> 
 				
 				<div class="col-sm-4">
-					<img src="img/7.jpg" class="img-fluid" alt="Responsive image">
+					<img src="img/7.jpg" class="img-fluid well" alt="Responsive image">
 				</div>
-				<div class="col-sm-4"><img src="img/8.jpg" class="img-fluid" alt="Responsive image"></div>
-				<div class="col-sm-4"><img src="img/9.jpg" class="img-fluid" alt="Responsive image"></div>
+				<div class="col-sm-4"><img src="img/8.jpg" class="img-fluid well" alt="Responsive image"></div>
+				<div class="col-sm-4"><img src="img/9.jpg" class="img-fluid well" alt="Responsive image"></div>
 				
 			</div>
 			
@@ -145,5 +114,6 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+		<script src="https://use.fontawesome.com/97a933fe64.js"></script>
 	</body>
 </html>
--- photography.php
+++ photography.php
@@ -1,3 +1,10 @@
+<?php 
+	
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -14,37 +21,8 @@
 		
 	</head>
 	<body>
-		<div class="container-fluid">
-			<nav class="navbar navbar-expand-lg navbar-light ">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-					<a class="navbar-brand" href="#">exclusivewallcovering</a>
-					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">ABOUT</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link " href="#">SERVICES</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">PHOTOGRAPHY</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link " href="#">LATEST NEWS</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link " href="#">CONTACT US</a>
-						</li>
-					</ul>
-					
-				</div>
-			</nav>
-		</div>
+	<?php include ("includes/menu.html");?>
+		
 		<div class="container"> 
 			<div class="row"> 
 				<div class="col-sm-3"> 
@@ -114,5 +92,6 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+		<script src="https://use.fontawesome.com/97a933fe64.js"></script>
 	</body>
 </html>
